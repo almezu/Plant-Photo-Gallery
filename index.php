5953d254a6f78581f64ffe0dd210131b1d6dd37e
almezu <74825011+almezu@users.noreply.github.com>
2020-11-24 16:23:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta name = "photogallery" content = "width=device-width, initial-scale=1.0">
+ <title> Flower Gallery </title>
+ <link rel="stylesheet" href="css/PHPstyle.css" type="text/css">
+ <meta charset="UTF-8">
+ </head> 
+ 
+ <body> 
+	
+	<h1> Flower Gallery </h1>
+	
+	<div class = "outer-grid">
+	
+		<div class = "inner-grid">
+			
+				<a href="Flower1.html">
+				<img src="img/Begonia.jpg"></img> </a>
+				
+				<a href="Flower2.html">
+				<img src="img/Camellia.jpg"></img> </a>
+				
+				<a href="Flower3.html">
+				<img src="img/Daffodil.jpg"></img> </a>
+				
+				<a href="Flower4.html">
+				<img src="img/Dahlia.jpg"></img> </a>
+				
+				<a href="Flower5.html">
+				<img src="img/Hydrangea.jpg"></img> </a>
+				
+				<a href="Flower6.html">
+				<img src="img/Iris.jpg"></img> </a>
+				
+				<a href="Flower7.html">
+				<img src="img/Plumeria.jpg"></img> </a>
+				
+				<a href="Flower8.html">
+				<img src="img/Stargazer_Lily.jpg"></img> </a>
+				
+				<a href="Flower9.html">
+				<img src="img/Sweet_Pea.jpg"></img> </a>
+				
+		</div>
+	</div>
+</body>
+</html>
